a9f4118a93fde134e9eb17cc26f0e86e99a3c2c5
KostYer <KostYer@yahoo.com>
2022-10-23 14:53:42 +0300

    debug point visualizer

---
+++ Assets/Scripts/Debug/PointGizmos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace DebugDraw
+{
+    public class PointGizmos : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position, 0.2f);
+        }
+    }
+}
