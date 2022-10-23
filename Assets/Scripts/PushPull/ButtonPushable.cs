3f333415bc47741f4b7b490ae80f1f8492f3b649
KostYer <KostYer@yahoo.com>
2022-10-23 14:52:19 +0300

    changed player prefab, added canvas with pushable button

---
+++ Assets/Scripts/PushPull/ButtonPushable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Player
+{
+    public class ButtonPushable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private Button _button;
+        public event Action OnButtonPressed;
+        public event Action OnButtonUnpressed;
+
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            OnButtonPressed?.Invoke();
+        }
+          
+          
+        
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+          {
+              OnButtonUnpressed?.Invoke();
+          }
+
+        public void Show(bool show)
+        {
+            _button.GetComponent<Image>().enabled = show;
+            _button.interactable = show;
+            
+            _button.transform.GetChild(0).gameObject.SetActive(show);
+        }
+    }
+}
