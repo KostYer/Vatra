ed6cdec8d54ef04c3e19bfdb595d0cf824020d53
KostYer <KostYer@yahoo.com>
2022-10-29 20:17:56 +0300

    events level start end

--- Assets/Scripts/GameCore/Context/GameContextInitializer.cs
+++ Assets/Scripts/GameCore/Context/GameContextInitializer.cs
@@ -1,21 +1,35 @@
-﻿using System;
+﻿
+using System;
+using Prefabs;
 using UnityEngine;
 
 namespace GameCore
 {
     public class GameContextInitializer : MonoBehaviour
     {
-        
-        
+        [SerializeField] private PrefabsProviderSO _prefabsProvider;
+        private SignalBus _signalBus;
+        private PrefabsSpawner _prefabsSpawner;
         private void Awake()
         {
             Initialize();
+            
+        }
+
+        private void Start()
+        {
+            _signalBus.Emit<ILevelStart>(s=>s.OnLevelStart());
         }
 
         private void Initialize()
         {
-            SignalBus eventBus = new SignalBus();
-            GameContext.Register<ISignalBus>(eventBus);
+            _signalBus  = new SignalBus();
+            _prefabsSpawner = new PrefabsSpawner();
+            
+            
+            GameContext.Register<ISignalBus>(_signalBus);
+            GameContext.Register<IPrefabsProvider>(_prefabsProvider);
+            GameContext.Register<IPrefabsSpawner>(_prefabsSpawner);
         }
     }
 }
--- Assets/Scripts/GameCore/Signals/IEventBusSubscriber.cs
+++ Assets/Scripts/GameCore/Signals/IEventBusSubscriber.cs
@@ -5,6 +5,16 @@
         
     }
     
+    public interface ILevelStart : IEventBusSubscriber
+    {
+        void OnLevelStart();
+    }
+    
+    public interface ILevelEnd : IEventBusSubscriber
+    {
+        void OnLevelEnd();
+    }
+    
     public interface IPlayerDied : IEventBusSubscriber
     {
         void OnPlayerDied();
